54a94e5d466ea04002ddddc70f6bfde43a89646b
jjlizcano <jjlizcano6@gmail.com>
2023-05-13 18:23:52 -0500

    Textos ajustados

---
+++ 3.ValorLlamada/Valor.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Valor{
+  public static void main(String[] args){
+    float tiempoLlamada;
+    float valorLlamada;
+
+    tiempoLlamada = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el tiempo de la llamada: "));
+
+    if(tiempoLlamada>3)
+    {
+      valorLlamada = 300 + 50*(tiempoLlamada-3);
+    }
+    else
+    {
+      valorLlamada = 300;
+    }
+
+    JOptionPane.showMessageDialog(null, "El valor de la llamada es: " + valorLlamada);
+  }
+}
