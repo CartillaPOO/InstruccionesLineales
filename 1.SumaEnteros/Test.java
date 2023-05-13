f270713fb5213e1526afd5cc8f2b032dd2bc8fab
jjlizcano <jjlizcano6@gmail.com>
2023-05-13 17:12:52 -0500

    Comentarios modificados

--- 1.SumaEnteros/Test.java
+++ 1.SumaEnteros/Test.java
@@ -6,25 +6,25 @@ public class Test
 {
     public static void main(String[] args)
     {
-        /*Declaración
-         * de
-         * variables
-        */
+        //
+        // Declaración de variables
+        //
+
         int x;
         int y;
         int z;
 
-        // input
+        // Input
         x = Integer.parseInt(JOptionPane.showInputDialog("Digite x: "));
         y = Integer.parseInt(JOptionPane.showInputDialog("Digite y: "));
 
-        // processing
+        // Processing
         z = x + y;
 
-        // output
-        JOptionPane.showMessageDialog(null, "La suma de " + x + " + " + y + " es: " + z, "Suma enteros", JOptionPane.ERROR_MESSAGE);
-        // PLAIN_MESSAGE para no mostrar ícono
+        // Output
+        JOptionPane.showMessageDialog(null, "La suma de " + x + " + " + y + " es: " + z, "Suma enteros", JOptionPane.PLAIN_MESSAGE);
+        // PLAIN_MESSAGE existe para no mostrar ningún ícono, pero existen íconos como ERROR_MESSAGE, INFORMATION_MESSAGE, WARNING_MESSAGE, QUESTION_MESSAGE
 
-        System.exit(0);
+        System.exit(0); // Termina la ejecución del programa
     }
 }
